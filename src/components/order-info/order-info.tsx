9c164a55ac72e11437c009e6e0d4bc0eb7eb7f13
Anton Belko <30528165+ko1p@users.noreply.github.com>
2021-11-14 12:54:17 +0300

    rename component order-details to order-info

--- src/components/order-info/order-info.tsx
+++ src/components/order-info/order-info.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from 'react';
-import styles from './order-details.module.css';
+import styles from './order-info.module.css';
 import orderAccepted from '../../images/order-accepted.gif'
 import { useSelector } from "react-redux";
 import { IStore, Info } from "../../types";
 
-export const OrderDetails: FC = () => {
+export const OrderInfo: FC = () => {
     const info = useSelector<IStore>(store => store.modal.data) as Info
     return (
         <div className={`${styles.container} pt-30 pr-25 pb-30 pl-25`}>
