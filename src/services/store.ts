5223dd529fef5492d847e975a87426f97dec9ee7
Anton Belko <30528165+ko1p@users.noreply.github.com>
2021-11-15 16:57:59 +0300

    remake store.js to store.ts

--- src/services/store.ts
+++ src/services/store.ts
@@ -4,10 +4,13 @@ import { rootReducer } from './reducers';
 import thunk from 'redux-thunk';
 import { socketMiddleware } from "./middleware/socketMiddleware";
 
-const composeEnhancers =
-    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-        : compose;
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancer = composeEnhancers(applyMiddleware(thunk, socketMiddleware(wsUrl)));
 
